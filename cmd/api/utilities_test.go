ac2625420bbded25df65bdbf79fb2c2871415004
petrostrak <pit.trak@gmail.com>
2022-12-13 20:29:15 +0200

    Add TestIsValidPeriod

---
+++ cmd/api/utilities_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestIsValidPeriod(t *testing.T) {
+	testCases := []struct {
+		name     string
+		period   string
+		expected bool
+	}{
+		{"hourly", "1h", true},
+		{"daily", "1d", true},
+		{"monthly", "1mo", true},
+		{"annually", "1y", true},
+		{"weekly", "1w", false},
+	}
+
+	for _, e := range testCases {
+		actual := isValidPeriod(e.period)
+
+		if actual != e.expected {
+			t.Errorf("Expected %v but got %v", e.expected, actual)
+		}
+	}
+}
